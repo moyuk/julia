2871c27cb43934ddf4efde7a98004d2be56ec316
Elliot Saba <staticfloat@gmail.com>
2020-04-08 16:58:39 -0700

    Hardening

--- contrib/relative_path.py
+++ contrib/relative_path.py
@@ -1,4 +1,8 @@
 import sys, os
+if len(sys.argv) != 3:
+    sys.stderr.write("\nrelative_path.py - incomplete arguments: %s\n"%(sys.argv))
+    sys.exit(1)
+
 sys.stdout.write(os.path.relpath(sys.argv[2], sys.argv[1]))
 
 # Always print a trailing slash
